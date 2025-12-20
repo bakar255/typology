1dc437c27e2abfbf24e5c5fab373404cb641d1ac
OfOluo <aboubakari.camarargy@gmail.com>
2025-12-20 16:00:18 +0100

    feat: add ButtonHeader component to handle mutiple title, props with chevron Icon

---
+++ src/components/ui/headerButton.tsx
@@ -0,0 +1,21 @@
+// Handle button's props with chevrow (title, content,  )
+
+import { ChevronDown } from "lucide-react";
+
+interface ButtonProps 
+
+    extends React.ButtonHTMLAttributes<HTMLButtonElement>{
+        title: string;
+        className?: string;
+    }
+
+    export function ButtonHeader({title, className}: ButtonProps) {
+        return(
+            <button className={`btn ${className} hover:bg-gray-50 transition-all duration-300 relative overflow-hidden group`} >
+                {title}
+                <ChevronDown size={16} className="inline-block ml-1 transition-transform duration-200 group-hover:rotate-180"/>
+                <span className="absolute bottom-0 left-full w-full h-0.5 bg-black transition-all duration-300 group-hover:left-0"></span>
+            </button>
+
+        )
+    }
